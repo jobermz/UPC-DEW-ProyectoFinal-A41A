7d7793e695502efabdc73d55258f605df0b49806
emersoncardenas <retemk@gmail.com>
2013-05-26 20:54:38 -0500

    avance HU02

--- social_red/app/models/district.rb
+++ social_red/app/models/district.rb
@@ -1,3 +1,5 @@
 class District < ActiveRecord::Base
   attr_accessible :district_name
+  has_many :locals
+  validates_presence_of :district_name, :message => '^ Debes ingresar un nombre de distrito'
 end
--- social_red/app/models/local.rb
+++ social_red/app/models/local.rb
@@ -1,4 +1,13 @@
 class Local < ActiveRecord::Base
-  belongs_to :district
+  attr_accessible :district_name, :district_id
+  belongs_to :district 
   attr_accessible :address, :description, :map_url, :phone
+  has_many :district
+
+
+  validates_presence_of :description, :message => '^ Debes ingresar una descricion'
+  validates_presence_of :address, :message => '^ Debes ingresar una direccion'
+  validates_presence_of :map_url, :message => '^ Debes ingresar un URL de google Maps'
+  validates_presence_of :phone, :message => '^ Debes ingresar un nombre de distrito'
+
 end
